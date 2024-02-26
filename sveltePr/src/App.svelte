cc464c6d52bfcee616302c404f2f1443ae62ce18
nguyenvietbinh <126882221+nguyenvietbinh@users.noreply.github.com>
2024-02-26 08:48:41 +0000

    as

--- sveltePr/src/App.svelte
+++ sveltePr/src/App.svelte
@@ -1,10 +1,16 @@
 <script>
-    import Background from "./background.svelte";
 </script>
 
+<img class="bg" src="img/background.png" alt="background">
 
-<Background/>
 
 <style>
-
+    .bg {
+        display: block;
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        top: 0%;
+        left: 0%;
+    }
 </style>
